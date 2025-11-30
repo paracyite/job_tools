1b8f81543898b211169565731d51775d718a5cf9
Louis <17791265+paracyite@users.noreply.github.com>
2025-11-29 23:06:43 -0500

    Delete resumegen.sh

--- resumegen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pandoc base.md \
-  --css=resumecss.css \
-  --pdf-engine=weasyprint \
-  --metadata pagetitle="Louis Pantuso" \
-  -o Louis_Pantuso_Resume.pdf
-
-echo "Done! -> LouisPantuso_Resume.pdf created"
